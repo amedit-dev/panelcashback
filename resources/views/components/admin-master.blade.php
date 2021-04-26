32ae9cfe6a85cc26e21a7e4f92399452eabcb839
amedit-dev <ameditmed@gmail.com>
2021-04-27 01:29:39 +0200

    added method edit and delete transactions fixed view screenshot (temp)

--- resources/views/components/admin-master.blade.php
+++ resources/views/components/admin-master.blade.php
@@ -14,7 +14,8 @@
     <link rel="apple-touch-icon" href="../../../app-assets/images/ico/apple-icon-120.png">
     <link rel="shortcut icon" type="image/x-icon" href="../../../app-assets/images/ico/favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
-
+    <link rel="shortcut icon" href="{{asset('app-assets/images/icons/favicon.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{asset('app-assets/images/icons/favicon.png')}}" type="image/x-icon">
     <!-- BEGIN: Vendor CSS-->
     <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/vendors.min.css">
     <link rel="stylesheet" type="text/css" href="../../../app-assets/vendors/css/tables/datatable/dataTables.bootstrap4.min.css">
